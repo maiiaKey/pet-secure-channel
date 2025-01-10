e54d534e7bc211a3a1e33641ca0c9bf2dae7c95e
Maiia Kuzmishyna <maiia.kuzmishyna@telusagcg.com>
2025-01-10 22:51:25 +0100

    added verify, decrypt to main and changed initial version to 0

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,48 @@ mod message;
 mod schnorr;
 mod serializers;
 
-
 fn main() {
-
     // Load the signing key from file
-    let signing_key = keys::KeyPair::from_file("signing_key.txt").expect("Failed to load signing key");
+    let signing_key =
+        keys::KeyPair::from_file("signing_key.txt").expect("Failed to load signing key");
 
     // Load the encryption public key from file
-    let encryption_key = keys::KeyPair::pk_from_file("encryption_key.txt").expect("Failed to load encryption key");
+    let encryption_key =
+        keys::KeyPair::pk_from_file("encryption_key.txt").expect("Failed to load encryption key");
 
     // Create a new message containing your group ID
-    let mut message = message::Message::new(1, b"Group ID: 21".to_vec(), 
-                                    signing_key.public_key.compress(), 
-                                    encryption_key.compress(), 
-                                    schnorr::SchnorrSignature::emty_signature());
+    let mut message = message::Message::new(
+        0,
+        b"Group ID: 21".to_vec(),
+        signing_key.public_key.compress(),
+        encryption_key.compress(),
+        schnorr::SchnorrSignature::emty_signature(),
+    );
 
     // Encrypt the message
-    message.encrypt(&encryption_key).expect("failed to encrypt the message");
+    message
+        .encrypt(&encryption_key)
+        .expect("failed to encrypt the message");
 
     // Sign the message
     message.sign(&signing_key.private_key);
 
     // Serialize the message for saving
-    let _ = message.to_file("signed_encrypted_message.json").expect("Failed to save message to file");
+    let _ = message
+        .to_file("signed_encrypted_message.json")
+        .expect("Failed to save message to file");
+
+    // Verification & decryption
+
+    println!();
+    println!("Verify message signature: {}", message.verify());
+
+    // let decryption_key: Scalar = <load-private-key>;
+    // message
+    //     .decrypt(&decryption_key)
+    //     .expect("Failed to decrypt message");
+
+    // println!();
+    // println!("Decrypted message:");
+    // message.display();
 }
-
-
